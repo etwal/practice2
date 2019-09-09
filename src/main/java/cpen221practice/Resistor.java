3a9d7ef80befdc52e64ba806d3390a456a77484e
Sathish Gopalakrishnan <sathish.gopalakrishnan@gmail.com>
2019-09-09 11:44:07 -0700

    initialized repo

---
+++ src/main/java/cpen221practice/Resistor.java
@@ -0,0 +1,11 @@
+package cpen221practice;
+
+class Resistor {
+    int colorCode(String color) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    String[] colors() {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+}
---
+++ src/test/java/cpen221practice/ResistorTest.java
@@ -0,0 +1,53 @@
+package cpen221practice;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Ignore;
+
+import static org.junit.Assert.assertEquals;
+
+public class ResistorTest {
+
+    private Resistor resistor;
+
+    @Before
+    public void setup() {
+        resistor = new Resistor();
+    }
+
+    @Test
+    public void testBlackColorCode() {
+        String input = "black";
+        int expected = 0;
+
+        Assert.assertEquals(expected, resistor.colorCode(input));
+    }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void testWhiteColorCode() {
+        String input = "white";
+        int expected = 9;
+
+        Assert.assertEquals(expected, resistor.colorCode(input));
+    }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void testOrangeColorCode() {
+        String input = "orange";
+        int expected = 3;
+
+        Assert.assertEquals(expected, resistor.colorCode(input));
+    }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void testColors() {
+        String[] expected = {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
+
+        Assert.assertEquals(expected, resistor.colors());
+    }
+    
+}
